e4d3b0a2b0415379e7f134de934eb72fda09eec0
Davi de Araújo Dias <107774203+diasdavizin@users.noreply.github.com>
2023-06-07 14:39:37 -0300

    Delete lista-estrutura-sequencial-python-br directory

--- lista-estrutura-sequencial-python-br/ex01.java
+++
@@ -1,8 +0,0 @@
-//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-package sequencialpythonbrasil;
-import java.util.Scanner;
-public class ex01 {
-    public static void main (String [] args){
-        System.out.println("Alo mundo");
-    }
-}
